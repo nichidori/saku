bcd358a0d1015741df38f35bb7ac3d8a9d19a11b
Arrafly Dori <arrafly.ad@gmail.com>
2025-08-17 21:09:23 +0700

    fix(desktop): set default window size

--- composeApp/src/desktopMain/kotlin/org/arraflydori/fin/main.kt
+++ composeApp/src/desktopMain/kotlin/org/arraflydori/fin/main.kt
@@ -1,16 +1,24 @@
 package org.arraflydori.fin
 
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import androidx.room.Room
 import org.arraflydori.fin.data.getRoomDatabase
 import org.arraflydori.fin.data.repo.DefaultAccountRepository
 import org.arraflydori.fin.data.repo.DefaultTrxRepository
 
 fun main() = application {
+    val state = rememberWindowState(size = DpSize(
+        width = 360.dp,
+        height = 720.dp,
+    ))
     Window(
         onCloseRequest = ::exitApplication,
         title = "Fin",
+        state = state,
     ) {
         // TODO: Build actual DB
         val db = getRoomDatabase(builder = Room.inMemoryDatabaseBuilder())
