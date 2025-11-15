83f9478c3ba2fa0bed0b908e8868e617728ea501
Arrafly Dori <arrafly.ad@gmail.com>
2025-11-15 19:11:22 +0700

    build: update version to `0.3.0+4`

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,8 +69,8 @@ android {
         applicationId = "dev.nichidori.saku"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.3.0"
     }
     lint {
         disable += "NullSafeMutableLiveData"
