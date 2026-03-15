efb7840e409a0062186c20a7ad8c27dcdd2c9ad7
Arrafly Dori <arraflydori@gmail.com>
2026-03-15 23:18:01 +0700

    build: update version to 0.8.1+13

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,8 +69,8 @@ android {
         applicationId = "dev.nichidori.saku"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "0.8.0"
+        versionCode = 13
+        versionName = "0.8.1"
     }
     lint {
         disable += "NullSafeMutableLiveData"
