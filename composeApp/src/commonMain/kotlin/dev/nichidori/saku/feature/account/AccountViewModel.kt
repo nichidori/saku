f8aaf98194b030958f6d40c559e0e0af01840c4f
Arrafly Dori <arraflydori@gmail.com>
2026-08-15 15:04:41 +0700

    fix: handle Account initial amount overwritten with current amount on save

--- composeApp/src/commonMain/kotlin/dev/nichidori/saku/feature/account/AccountViewModel.kt
+++ composeApp/src/commonMain/kotlin/dev/nichidori/saku/feature/account/AccountViewModel.kt
@@ -21,6 +21,7 @@ data class AccountUiState(
     val isLoading: Boolean = false,
     val name: String = "",
     val balance: Long? = null,
+    val initialAmount: Long? = null,
     val limit: Long? = null,
     val type: AccountType? = null,
     val isEditing: Boolean = false,
@@ -61,6 +62,7 @@ class AccountViewModel(
                         isLoading = false,
                         name = account?.name ?: credit?.name.orEmpty(),
                         balance = account?.currentAmount ?: credit?.currentAmount,
+                        initialAmount = account?.initialAmount,
                         limit = credit?.limit,
                         type = if (credit != null) AccountType.Credit else account?.type,
                         isEditing = account != null || credit != null
@@ -127,7 +129,7 @@ class AccountViewModel(
                         accountRepository.updateAccount(
                             id = id,
                             name = uiState.value.name,
-                            initialAmount = uiState.value.balance!!,
+                            initialAmount = uiState.value.initialAmount ?: uiState.value.balance!!,
                             type = uiState.value.type!!
                         )
                     } else {
