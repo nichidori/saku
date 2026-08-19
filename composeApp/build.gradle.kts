6cc3f1a6265273320016951c616112212104186e
Arrafly Dori <arraflydori@gmail.com>
2026-08-19 23:32:16 +0700

    build: update version to 0.24.1+42

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
     alias(libs.plugins.buildconfig)
 }
 
-val appVersionName = "0.24.0"
-val appVersionCode = 41
+val appVersionName = "0.24.1"
+val appVersionCode = 42
 
 buildConfig {
     packageName("dev.nichidori.saku.composeApp")
